048e3bd017d2999f0a2cf1a8d3f16a4d2665d250
Jiranathanan Siripun <jiranathanan@gmail.com>
2025-08-03 16:22:18 +0700

    Add button & signal

--- World.gd
+++ World.gd
@@ -3,10 +3,13 @@ extends Node
 onready var animationPlayer : AnimationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	animationPlayer.play("Launch")
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_LaunchButton_pressed():
+	animationPlayer.play("Launch")
